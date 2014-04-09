0610a0b29f25bca9365a6d2d9f88e6bde204f470
Chris West <cwest@appnexus.com>
2014-04-09 14:16:00 -0400

    added robot js page controller to put our logic in...

---
+++ js/robot-pc.js
@@ -0,0 +1,18 @@
+var robot;
+
+(function($) {
+	robot = function() {
+
+		var _init = function() {
+			console.log("robot's got a heartbeat");
+		};
+
+		_init();
+
+	};
+
+	return robot;
+
+})(jQuery);
+
+
