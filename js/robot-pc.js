a1b018156f4cf5e5779658418e5a79bda6ce3d28
Chris West <cwest@appnexus.com>
2014-04-09 15:28:38 -0400

    added animation routine that is now in a settimeout

--- js/robot-pc.js
+++ js/robot-pc.js
@@ -1,10 +1,44 @@
 var robot;
 
 (function($) {
-	robot = function() {
-
+	robot = function(config) {
+		var _config = {
+			animationRoutine : ['lazy'], // he will just have one lazy move
+			animationLength : 10000, // length of the animation play time
+			$robot : null // parent jquery robot object
+		};
 		var _init = function() {
 			console.log("robot's got a heartbeat");
+			$.extend(_config, config);
+			console.log(_config.animationRoutine);
+
+			_bindRobot();
+
+		};
+
+		var _bindRobot = function() {
+			$(document).keyup(function(e) {
+				if(e.keyCode == 32) {
+					_startRobotAnimation();
+				}
+			});
+		};
+
+		var _startRobotAnimation = function() {
+			var timer = Math.floor(_config.animationLength / _config.animationRoutine.length);
+			console.log('starting animation: ' + timer);
+			var routine = _config.animationRoutine;
+			for (i = 0; i < routine.length; i++) {
+				(function(thisRoutine, intv) {
+					setTimeout(function() {
+						if(intv > 0) {
+							_config.$robot.removeClass(thisRoutine[intv-1]);
+						}
+						_config.$robot.addClass(thisRoutine[intv]);
+					}, timer * (i));
+				})(routine, i);
+			}
+
 		};
 
 		_init();
