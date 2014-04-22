4df22b653839ec8450d0033926ad31022f6a550f
Chris West <cwest@appnexus.com>
2014-04-22 07:43:14 -0400

    added library to the left of the stage, and added and refined many animations, also added the title sequence.

--- js/robot-pc.js
+++ js/robot-pc.js
@@ -2,9 +2,11 @@ var robot;
 
 (function($) {
 	robot = function(config) {
+		var _$slideMenu = $(".moves");
+		var _$title = $("h1.title");
 		var _config = {
 			animationRoutine : ['lazy'], // he will just have one lazy move
-			animationLength : 6000, // length of the animation play time
+			animationLength : 10000, // length of the animation play time
 			$robot : null // parent jquery robot object
 		};
 		var _init = function() {
@@ -22,12 +24,31 @@ var robot;
 					_startRobotAnimation();
 				}
 			});
+			_$slideMenu.find(".handle").on('click', function() {
+				_$slideMenu.toggleClass("open");
+			});
+			_$slideMenu.find("p").on('click', _applySTaticStyle);
+		};
+
+		var _applySTaticStyle = function(e) {
+			var thisClick = $(e.currentTarget);
+			var thisStyle = thisClick.attr('data-classname');
+			var amremoving = thisClick.attr('data-remover');
+			_config.$robot.removeClass().addClass('body ' + thisStyle);
+			if(amremoving == 'true') {
+				setTimeout(function() {
+					_config.$robot.removeClass().addClass('body ');
+				}, 700);
+			}
 		};
 
 		var _startRobotAnimation = function() {
 			var timer = Math.floor(_config.animationLength / _config.animationRoutine.length);
 			$(".stage div").css('transition','all ' + timer + 'ms ease-in-out');
 			var routine = _config.animationRoutine;
+
+			_$title.addClass('fadeIn');
+			setTimeout(function() { _$title.addClass('fadeOut'); }, timer*routine.length/2);
 			for (i = 0; i < routine.length; i++) {
 				(function(thisRoutine, intv) {
 					setTimeout(function() {
