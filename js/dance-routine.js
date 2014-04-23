d198f5d279332e53892cc9440a960b58c9599c66
Nathaniel Lee <nlee@appnexus.com>
2014-04-23 12:35:27 -0400

    Updates to the DanceRoutine class to alter the array of dance steps based on interactions from the user.

--- js/dance-routine.js
+++ js/dance-routine.js
@@ -2,17 +2,16 @@
 	var uid = 0;
 
 	function DanceRoutine(params) {
-		var _danceSteps;
+		var _mappedSteps;
 		var _$root;
 
 		function _init(params) {
 			if (typeof params !== 'object') {
 				params = {};
 			}
-			_danceSteps = params.danceSteps || [];
+			_mappedSteps = _mapSteps(params.danceSteps || []);
 			_$root = $('<div class="DanceRoutine"/>');
 			_render();
-			_bindEvents();
 		}
 
 		function _get$Root() {
@@ -20,64 +19,77 @@
 		}
 
 		function _getDanceSteps() {
-			return _danceSteps;
+			var steps = [];
+
+			Object.keys(_mappedSteps).forEach(function (stepId) {
+				var moves = [];
+
+				Object.keys(_mappedSteps[stepId].moves).forEach(function (moveId) {
+					moves.push(_mappedSteps[stepId].moves[moveId].name);
+				});
+				steps.push(moves.join(' '));
+			});
+			return steps;
+		}
+
+		function _mapSteps(steps) {
+			var map = {};
+
+			steps.forEach(function (step) {
+				var stepId = ++uid;
+
+				map[stepId] = { moves: {} };
+				step.split(' ').forEach(function (move) {
+					var moveId = ++uid;
+
+					map[stepId].moves[moveId] = {
+						name: move
+					};
+				});
+			});
+
+			return map;
 		}
 
 		function _render() {
 			var $list = $('<ul/>');
 
-			_danceSteps.forEach(function (step) {
-				var stepId = ++uid;
+			Object.keys(_mappedSteps).forEach(function (stepId) {
+				var step = _mappedSteps[stepId];
 				var $step = $(
 					'<li class="step step-' + stepId + ' removable">' +
 						'<ul/>' +
 						'<span class="remove">X</span>' +
 					'</li>'
-				);
+				).appendTo($list);
 
-				step.split(' ').forEach(function (move) {
-					var moveId = ++uid;
-					$step.find('ul').append(
+				Object.keys(step.moves).forEach(function (moveId) {
+					var move = step.moves[moveId];
+					var $move = $(
 						'<li class="move move-' + moveId + ' removable">' +
-							'<span>' + move + '</span>' +
+							'<span>' + move.name + '</span>' +
 							'<span class="remove">X</span>' +
 						'</li>'
-					);
+					).appendTo($step).on('click', '.remove', function () {
+						$move.addClass('removed');
+						window.setTimeout(function () {
+							_deleteMove(stepId, moveId);
+							_render();
+						}, 300);
+					});
+
+					move.$el = $move;
 				});
-				$list.append($step);
+				step.$el = $step;
 			});
-			_$root.append($list);
+			_$root.empty().append($list);
+
+			console.log(_getDanceSteps());
 		}
 
-		function _getTargetType($el) {
-			if ($el.hasClass('move')) {
-				return 'move';
-			} else if ($el.hasClass('step')) {
-				return 'step';
-			} else {
-				return null;
-			}
-		}
-
-		function _bindEvents() {
-			_$root.on('click', '.remove', function (e) {
-				var $target = $(e.target).parent().addClass('removed');
-				var type = _getTargetType($target);
-
-				if (type === 'move') {
-					_deleteMove(id);
-				} else if (type === 'step') {
-					_deleteStep(id);
-				}
-			});
-		}
-
-		function _deleteMove(id) {
-			// 
-		}
-
-		function _deleteStep(id) {
-			// 
+		function _deleteMove(stepId, moveId) {
+			_mappedSteps[stepId].moves[moveId].$el.remove();
+			delete _mappedSteps[stepId].moves[moveId];
 		}
 
 		this.getDanceSteps = _getDanceSteps;
